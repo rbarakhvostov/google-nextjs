8bf301fdf5d1d2029fb634697259baf6545ed6dc
rbarakhvostov <rbarakhvostov@gmail.com>
2024-03-29 00:19:32 +0300

    add home body

---
+++ src/components/HomeSearch.js
@@ -0,0 +1,64 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsFillMicFill } from 'react-icons/bs';
+
+export default function HomeSearch() {
+  const [input, setInput] = useState('');
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+  const router = useRouter();
+
+  const handleChange = (e) => setInput(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+
+  const randomSearch = async () => {
+    setRandomSearchLoading(true);
+
+    const response = await fetch('https://random-word-api.herokuapp.com/word');
+    const result = await response.json();
+
+    setRandomSearchLoading(false);
+    router.push(`/search/web?searchTerm=${result}`);
+  }
+
+  return (
+    <>
+      <form
+        className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+        onSubmit={handleSubmit}
+      >
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input
+          type="text"
+          className="flex-grow focus: outline-none"
+          value={input}
+          onChange={handleChange}
+        />
+        <BsFillMicFill className="text-lg" />
+      </form>
+      <div className="flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-8 sm:space-x-4">
+        <button
+          className="bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow"
+          onClick={handleSubmit}
+        >
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          className="bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow disabled:opacity-80 disabled:shadow-sm"
+          onClick={randomSearch}
+        >
+          {randomSearchLoading ? "Loading..." : "I am feeling lucky"}
+        </button>
+      </div>
+    </>
+  )
+}
