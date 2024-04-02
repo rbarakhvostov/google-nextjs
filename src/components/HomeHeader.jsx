705f1c0dc22841e8a0fa515b46192bf6a6412daa
rbarakhvostov <rbarakhvostov@gmail.com>
2024-04-02 14:17:02 +0300

    update the search header

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -18,10 +18,9 @@ export default function HomeHeader() {
           Images
         </Link>
         <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
-        <button
-          className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
-            Sign in
-          </button>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
+          Sign in
+        </button>
       </div>
     </header>
   )
