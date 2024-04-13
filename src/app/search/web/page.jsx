b9bb93a214534b7fe22245d55db87f07f006242d
rbarakhvostov <rbarakhvostov@gmail.com>
2024-04-13 15:02:47 +0300

    connect to google api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,12 @@
-export default function WebSearchPage({ children }) {
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`)
+  const { items } = await response.json()
+
   return (
     <div>
-      
+      {items.map((item) => (
+        <h1 key={item.cacheId}>{item.title}</h1>
+      ))}
     </div>
   );
 }
